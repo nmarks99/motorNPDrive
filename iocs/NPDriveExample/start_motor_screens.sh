6db665cd95a14064f2600dd1dc3784c65528f6b1
nmarks99 <nmarks@anl.gov>
2024-10-02 14:06:39 -0500

    update screens, add example start_caQtDM_NPDrive script

--- iocs/NPDriveExample/start_motor_screens.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-export CAQTDM_DISPLAY_PATH=$CAQTDM_DISPLAY_PATH:/APSshare/epics/synApps_6_3/support/motor-R7-3-1/motorApp/op/ui
-export CAQTDM_DISPLAY_PATH=$CAQTDM_DISPLAY_PATH:/APSshare/epics/synApps_6_3/support/motor-R7-3-1/motorApp/op/ui/autoconvert
-/APSshare/bin/caQtDM -macro P=NPDriveExample:,M1=m1,M2=m2,M3=m3,M4=m4,M5=m5,M6=m6,M7=m7,M8=m8 /APSshare/epics/synApps_6_3/support/motor-R7-3-1/motorApp/op/ui/autoconvert/topMotors8.ui
